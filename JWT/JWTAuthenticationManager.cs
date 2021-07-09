d33251a26bcb2311178cbc2616d5580bdec619e5
Arturo Daza <carlos.arturo.dazab@gmail.com>
2021-07-09 11:10:35 -0400

    -r2d2 was here

--- JWT/JWTAuthenticationManager.cs
+++ JWT/JWTAuthenticationManager.cs
@@ -1,12 +1,9 @@
 ﻿using Microsoft.IdentityModel.Tokens;
+using MttoApi.Model;
 using System;
-using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
 using System.Security.Claims;
 using System.Text;
-
-using MttoApi.Model;
 
 namespace MttoApi
 {
@@ -17,11 +14,10 @@ namespace MttoApi
 
     public class JWTAuthenticationManager : IJWTAuthenticationManager
     {
-        
         //CLAVE SECRETA PARA LA ENCRIPTACION
         private readonly string key = string.Empty;
 
-        //CONSTRUCTOR DE LA CLASE 
+        //CONSTRUCTOR DE LA CLASE
         public JWTAuthenticationManager(string secretkey)
         {
             this.key = secretkey;
@@ -30,7 +26,7 @@ namespace MttoApi
         //METODO DE AUTENTICACION
         public string Authenticate(LogInRequest request)
         {
-            //CREAMOS Y DEFINIMOS LAS VARIABLES QUE FUNCIONARAN PARA CONFIGURAR LA INFORMACION DEL TOKEN A GENERAR 
+            //CREAMOS Y DEFINIMOS LAS VARIABLES QUE FUNCIONARAN PARA CONFIGURAR LA INFORMACION DEL TOKEN A GENERAR
             var tokenHandler = new JwtSecurityTokenHandler();
             var tokenKey = Encoding.ASCII.GetBytes(key);
             var tokenDescription = new SecurityTokenDescriptor
